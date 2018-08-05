cd24f9b6d0f1ad2b8779867b88a73d7476b066b0
Premysl Donat <pdonat@seznam.cz>
2018-08-05 21:18:07 +0200

    Initial commit

--- lib/money_xml_validator.rb
+++ lib/money_xml_validator.rb
@@ -1,5 +1,18 @@
-require "money_xml_validator/version"
+require 'nokogiri'
 
 module MoneyXmlValidator
-  # Your code goes here...
+  def self.validate(utf_source)
+    gem_root = File.expand_path("../..", __FILE__)
+
+    path = "#{gem_root}/lib/money_xml_validator/money_xsd/_Document.xsd"
+    io = File.open(path)
+    schema = Nokogiri::XML::Schema(io)
+
+    doc = Nokogiri::XML(utf_source)
+
+
+    errors = schema.validate(doc)
+
+    errors
+  end
 end
--- lib/money_xml_validator/version.rb
+++
@@ -1,3 +0,0 @@
-module MoneyXmlValidator
-  VERSION = "0.1.0"
-end
