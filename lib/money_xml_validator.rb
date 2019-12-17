c7aa57ce606ebc1e6f921905dfd0c02545f0fab9
Premysl Donat <pdonat@seznam.cz>
2019-12-17 21:50:50 +0100

    Fix schema path

--- lib/money_xml_validator.rb
+++ lib/money_xml_validator.rb
@@ -4,7 +4,7 @@ module MoneyXmlValidator
   def self.validate(utf_source)
     gem_root = File.expand_path("../..", __FILE__)
 
-    path = "#{gem_root}/lib/money_xml_validator/money_xsd/_Document.xsd"
+    path = "#{gem_root}/lib/money_xml_validator/money_xsd/schemas/_Document.xsd"
     io = File.open(path)
     schema = Nokogiri::XML::Schema(io)
 
